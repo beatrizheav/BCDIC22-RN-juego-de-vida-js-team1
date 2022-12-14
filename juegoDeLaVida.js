b61abb95fb7eed483a507be3c7dc437ef83010bf
Beatrizavila <iheredia@ucol.mx>
2022-12-14 17:59:20 -0600

    Funcional.
    Co-authored-by: github <EliasCode79>
    Co-authored-by: github <Camilapinz18>
    Co-authored-by: github <garzo94>
    Co-authored-by: github <GaddPat609>

--- juegoDeLaVida.js
+++ juegoDeLaVida.js
@@ -2,7 +2,17 @@
 const fila = process.argv[2];
 const columnas = process.argv[3];
 
-const myarray = [];
+const primeraGeneracion = [];
+const siguienteGeneracion = [];
+
+for (let n = 0; n < fila; n++) {
+  primeraGeneracion[n] = [];
+  siguienteGeneracion[n] = [];
+  for (let m = 0; m < columnas; m++) {
+    primeraGeneracion[n][m] = llenarCampos();
+    siguienteGeneracion[n][m] = primeraGeneracion[n][m];
+  }
+}
 
 function llenarCampos() {
   const valor = Math.round(Math.random());
@@ -10,14 +20,7 @@ function llenarCampos() {
   return ".";
 }
 
-for (let n = 0; n < fila; n++) {
-  myarray[n] = [];
-  for (let m = 0; m < columnas; m++) {
-    myarray[n][m] = llenarCampos();
-  }
-}
-
-console.table(myarray);
+// let siguienteGeneracion = [...primeraGeneracion];
 
 // recorrido del array
 function recorrido() {
@@ -28,366 +31,395 @@ function recorrido() {
   }
 }
 
+function estadoActual(n,m){
+  if(primeraGeneracion[n][m] === '.'){
+    return false
+  }else { return true}
+}
+
 function ubicacion(n, m) {
   if (n === 0 && m === 0) {
-    console.log("esquina superior izquierda");
     esquinaSupIzq(n, m);
   } else if (n === 0 && m === columnas - 1) {
-    console.log("esquina superior derecha");
     esquinaSupDer(n, m);
   } else if (n === fila - 1 && m === 0) {
-    console.log("esquina inferior izquierda");
     esquinaInfIzq(n, m);
   } else if (n === fila - 1 && m === columnas - 1) {
-    console.log("esquina inferior derecha");
     esquinaInfDer(n, m);
   } else if (n === 0) {
-    console.log("es borde superior");
     borderSup(n, m);
   } else if (n === fila - 1) {
-    console.log("borde inferior");
     borderInf(n, m);
   } else if (m === 0) {
-    console.log("borde izquierdo");
     borderIzq(n, m);
   } else if (m === columnas - 1) {
-    console.log("borde derecho");
     borderDer(n, m);
   } else {
-    console.log("centro");
     centro(n, m);
   }
 }
+
+function creacionSiguienteGeneracion(vivas,muertas,r,c) {
+  const estado = estadoActual(r,c)
+
+  //Condición 1 y 2
+  if ((estado === true) && (vivas < 2 || vivas >3)) {
+    siguienteGeneracion[r][c] = '.'
+  } 
+  // Condición 3
+  else if ((estado === true) && (vivas === 2 || vivas === 3)){
+    siguienteGeneracion[r][c] = '*'
+  }
+  //Condicion 4
+  else if ((estado === false) && (vivas === 3)){
+    siguienteGeneracion[r][c] = '*'
+  }
+} 
 
 recorrido();
 
 function esquinaSupIzq(r, c) {
   let vivas = 0;
   let muertas = 0;
-  if (myarray[r + 1][c] === ".") {
+  if (primeraGeneracion[r + 1][c] === ".") {
     muertas++;
   } else {
     vivas++;
   }
 
-  if (myarray[r + 1][c + 1] === ".") {
+  if (primeraGeneracion[r + 1][c + 1] === ".") {
     muertas++;
   } else {
     vivas++;
   }
 
-  if (myarray[r][c + 1] === ".") {
+  if (primeraGeneracion[r][c + 1] === ".") {
     muertas++;
   } else {
     vivas++;
   }
 
-  console.log(`Vivas ${vivas}`);
-  console.log(`Muertas ${muertas}`);
+  creacionSiguienteGeneracion(vivas,muertas,r,c);
+
+  // console.log(`Vivas ${vivas}`);
+  // console.log(`Muertas ${muertas}`);
 }
 
 function esquinaSupDer(r, c) {
   let vivas = 0;
   let muertas = 0;
-  if (myarray[r][c - 1] === ".") {
+  if (primeraGeneracion[r][c - 1] === ".") {
     muertas++;
   } else {
     vivas++;
   }
 
-  if (myarray[r + 1][c] === ".") {
+  if (primeraGeneracion[r + 1][c] === ".") {
     muertas++;
   } else {
     vivas++;
   }
 
-  if (myarray[r + 1][c - 1] === ".") {
+  if (primeraGeneracion[r + 1][c - 1] === ".") {
     muertas++;
   } else {
     vivas++;
   }
 
-  console.log(`Vivas ${vivas}`);
-  console.log(`Muertas ${muertas}`);
+  creacionSiguienteGeneracion(vivas,muertas,r,c);
+  // console.log(`Vivas ${vivas}`);
+  // console.log(`Muertas ${muertas}`);
 }
 
 function esquinaInfIzq(r, c) {
   let vivas = 0;
   let muertas = 0;
-  if (myarray[r - 1][c] === ".") {
+  if (primeraGeneracion[r - 1][c] === ".") {
     muertas++;
   } else {
     vivas++;
   }
 
-  if (myarray[r - 1][c + 1] === ".") {
+  if (primeraGeneracion[r - 1][c + 1] === ".") {
     muertas++;
   } else {
     vivas++;
   }
 
-  if (myarray[r][c + 1] === ".") {
+  if (primeraGeneracion[r][c + 1] === ".") {
     muertas++;
   } else {
     vivas++;
   }
 
-  console.log(`Vivas ${vivas}`);
-  console.log(`Muertas ${muertas}`);
+  creacionSiguienteGeneracion(vivas,muertas,r,c);
+  // console.log(`Vivas ${vivas}`);
+  // console.log(`Muertas ${muertas}`);
 }
 
 function esquinaInfDer(r, c) {
   let vivas = 0;
   let muertas = 0;
-  if (myarray[r - 1][c] === ".") {
+  if (primeraGeneracion[r - 1][c] === ".") {
     muertas++;
   } else {
     vivas++;
   }
 
-  if (myarray[r - 1][c - 1] === ".") {
+  if (primeraGeneracion[r - 1][c - 1] === ".") {
     muertas++;
   } else {
     vivas++;
   }
 
-  if (myarray[r][c - 1] === ".") {
+  if (primeraGeneracion[r][c - 1] === ".") {
     muertas++;
   } else {
     vivas++;
   }
 
-  console.log(`Vivas ${vivas}`);
-  console.log(`Muertas ${muertas}`);
+  creacionSiguienteGeneracion(vivas,muertas,r,c);
+  // console.log(`Vivas ${vivas}`);
+  // console.log(`Muertas ${muertas}`);
 }
 
 function borderSup(r, c) {
   let vivas = 0;
-  let muerta = 0;
+  let muertas = 0;
   // lado izquierdo
-  if (myarray[r][c - 1] === ".") {
-    muerta++;
+  if (primeraGeneracion[r][c - 1] === ".") {
+    muertas++;
   } else {
     vivas++;
   }
 
   // lado derecho
-  if (myarray[r][c + 1] === ".") {
-    muerta++;
+  if (primeraGeneracion[r][c + 1] === ".") {
+    muertas++;
   } else {
     vivas++;
   }
 
   // lado abajo
-  if (myarray[r + 1][c] === ".") {
-    muerta++;
+  if (primeraGeneracion[r + 1][c] === ".") {
+    muertas++;
   } else {
     vivas++;
   }
 
   // diagonal izquierda
-  if (myarray[r + 1][c - 1] === ".") {
-    muerta++;
+  if (primeraGeneracion[r + 1][c - 1] === ".") {
+    muertas++;
   } else {
     vivas++;
   }
 
   // diagonal derecha
-  if (myarray[r + 1][c + 1] === ".") {
-    muerta++;
+  if (primeraGeneracion[r + 1][c + 1] === ".") {
+    muertas++;
   } else {
     vivas++;
   }
 
-  console.log(`vivasssss ${vivas}`);
-  console.log(`muertassss ${muerta}`);
+  creacionSiguienteGeneracion(vivas,muertas,r,c);
+  // console.log(`vivasssss ${vivas}`);
+  // console.log(`muertassss ${muertas}`);
 }
 
 function borderInf(r, c) {
   let vivas = 0;
-  let muerta = 0;
+  let muertas = 0;
   // lado izquierdo
-  if (myarray[r][c - 1] === ".") {
-    muerta++;
+  if (primeraGeneracion[r][c - 1] === ".") {
+    muertas++;
   } else {
     vivas++;
   }
 
   // lado derecho
-  if (myarray[r][c + 1] === ".") {
-    muerta++;
+  if (primeraGeneracion[r][c + 1] === ".") {
+    muertas++;
   } else {
     vivas++;
   }
 
   // lado arriba
-  if (myarray[r - 1][c] === ".") {
-    muerta++;
+  if (primeraGeneracion[r - 1][c] === ".") {
+    muertas++;
   } else {
     vivas++;
   }
 
   // diagonal izquierda
-  if (myarray[r - 1][c - 1] === ".") {
-    muerta++;
+  if (primeraGeneracion[r - 1][c - 1] === ".") {
+    muertas++;
   } else {
     vivas++;
   }
 
   // diagonal derecha
-  if (myarray[r - 1][c + 1] === ".") {
-    muerta++;
+  if (primeraGeneracion[r - 1][c + 1] === ".") {
+    muertas++;
   } else {
     vivas++;
   }
 
-  console.log(`vivasssss ${vivas}`);
-  console.log(`muertassss ${muerta}`);
+  creacionSiguienteGeneracion(vivas,muertas,r,c);
+  // console.log(`vivasssss ${vivas}`);
+  // console.log(`muertassss ${muertas}`);
 }
 
 function borderIzq(r, c) {
   let vivas = 0;
-  let muerta = 0;
+  let muertas = 0;
   // lado arriba
-  if (myarray[r - 1][c] === ".") {
-    muerta++;
+  if (primeraGeneracion[r - 1][c] === ".") {
+    muertas++;
   } else {
     vivas++;
   }
 
   // lado diagonal arriba
-  if (myarray[r - 1][c + 1] === ".") {
-    muerta++;
+  if (primeraGeneracion[r - 1][c + 1] === ".") {
+    muertas++;
   } else {
     vivas++;
   }
 
   // lado derecho
-  if (myarray[r][c + 1] === ".") {
-    muerta++;
+  if (primeraGeneracion[r][c + 1] === ".") {
+    muertas++;
   } else {
     vivas++;
   }
 
   // diagonal abajo
-  if (myarray[r + 1][c + 1] === ".") {
-    muerta++;
+  if (primeraGeneracion[r + 1][c + 1] === ".") {
+    muertas++;
   } else {
     vivas++;
   }
 
   // lado abjo
-  if (myarray[r + 1][c] === ".") {
-    muerta++;
+  if (primeraGeneracion[r + 1][c] === ".") {
+    muertas++;
   } else {
     vivas++;
   }
 
-  console.log(`vivasssss ${vivas}`);
-  console.log(`muertassss ${muerta}`);
+  creacionSiguienteGeneracion(vivas,muertas,r,c);
+  // console.log(`vivasssss ${vivas}`);
+  // console.log(`muertassss ${muertas}`);
 }
 
 function borderDer(r, c) {
   let vivas = 0;
-  let muerta = 0;
+  let muertas = 0;
   // lado arriba
-  if (myarray[r - 1][c] === ".") {
-    muerta++;
+  if (primeraGeneracion[r - 1][c] === ".") {
+    muertas++;
   } else {
     vivas++;
   }
 
   //diagonal izquierda arriba
-  if (myarray[r - 1][c - 1] === ".") {
-    muerta++;
+  if (primeraGeneracion[r - 1][c - 1] === ".") {
+    muertas++;
   } else {
     vivas++;
   }
 
   // lado izauierdo
-  if (myarray[r][c - 1] === ".") {
-    muerta++;
+  if (primeraGeneracion[r][c - 1] === ".") {
+    muertas++;
   } else {
     vivas++;
   }
 
   // diagonal izauierdo abajo
-  if (myarray[r + 1][c - 1] === ".") {
-    muerta++;
+  if (primeraGeneracion[r + 1][c - 1] === ".") {
+    muertas++;
   } else {
     vivas++;
   }
 
   // lado abjo
-  if (myarray[r + 1][c] === ".") {
-    muerta++;
+  if (primeraGeneracion[r + 1][c] === ".") {
+    muertas++;
   } else {
     vivas++;
   }
 
-  console.log(`vivasssss ${vivas}`);
-  console.log(`muertassss ${muerta}`);
+  creacionSiguienteGeneracion(vivas,muertas,r,c);
+  // console.log(`vivasssss ${vivas}`);
+  // console.log(`muertassss ${muertas}`);
 }
 
 function centro(r, c) {
   let vivas = 0;
-  let muerta = 0;
+  let muertas = 0;
   // lado arriba
-  if (myarray[r - 1][c] === ".") {
-    muerta++;
+  if (primeraGeneracion[r - 1][c] === ".") {
+    muertas++;
   } else {
     vivas++;
   }
 
   // Diagonal arribal derecha
-  if (myarray[r - 1][c + 1] === ".") {
-    muerta++;
+  if (primeraGeneracion[r - 1][c + 1] === ".") {
+    muertas++;
   } else {
     vivas++;
   }
 
   // lado derecho
-  if (myarray[r][c + 1] === ".") {
-    muerta++;
+  if (primeraGeneracion[r][c + 1] === ".") {
+    muertas++;
   } else {
     vivas++;
   }
 
   // Diagonal abajo derecho
-  if (myarray[r + 1][c + 1] === ".") {
-    muerta++;
+  if (primeraGeneracion[r + 1][c + 1] === ".") {
+    muertas++;
   } else {
     vivas++;
   }
 
   // lado abajo
-  if (myarray[r + 1][c] === ".") {
-    muerta++;
+  if (primeraGeneracion[r + 1][c] === ".") {
+    muertas++;
   } else {
     vivas++;
   }
 
   // Diagonal abajo izquirda
-  if (myarray[r + 1][c - 1] === ".") {
-    muerta++;
+  if (primeraGeneracion[r + 1][c - 1] === ".") {
+    muertas++;
   } else {
     vivas++;
   }
 
   // lado isquierdo
-  if (myarray[r][c - 1] === ".") {
-    muerta++;
+  if (primeraGeneracion[r][c - 1] === ".") {
+    muertas++;
   } else {
     vivas++;
   }
 
   // Diagonal arriba izquirda
-  if (myarray[r - 1][c - 1] === ".") {
-    muerta++;
+  if (primeraGeneracion[r - 1][c - 1] === ".") {
+    muertas++;
   } else {
     vivas++;
   }
 
-  console.log(`vivasssss ${vivas}`);
-  console.log(`muertassss ${muerta}`);
+  creacionSiguienteGeneracion(vivas,muertas,r,c);
+  // console.log(`vivasssss ${vivas}`);
+  // console.log(`muertassss ${muertas}`);
 }
+
+console.log('Primera Generación')
+console.table(primeraGeneracion);
+console.log('Segunda Generación')
+console.table(siguienteGeneracion);
